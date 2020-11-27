93f02296871c4c1bb25a06086a9542656ce04fd5
magedibr <magedibr@sheridancollege.ca>
2020-11-26 19:45:31 -0500

    1st

--- src/main/java/MidtermAMohamed/controllers/HomeController.java
+++ src/main/java/MidtermAMohamed/controllers/HomeController.java
@@ -1,32 +1,77 @@
 package MidtermAMohamed.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import MidtermAMohamed.database.DatabaseAccess;
 
 @Controller
 public class HomeController {
-
 	
+	
+	
+	
+	
+	@Lazy
+	@Autowired
+	private DatabaseAccess da;
+	
+	
+
 	@GetMapping("/")
 	public String index() {
 		return "index";
 	}
-	
-	
+
+
 	@GetMapping("/secure")
 	public String secureIndex() {
 		return "/secure/index";
 	}
-	
+
 	@GetMapping("/login")
 	public String login() {
 		return "login";
 	}
-	
+
 	@GetMapping("/permission-denied")
-	public String permissionDenied() {
+	public String permissionDenied()
+	{
 		return "/error/permission-denied";
 	}
+
+	@GetMapping("/register") 
+	public String getRegister () 
+	{ 
+		return "register"; 
+	}
+
+	@PostMapping("/register")
+	public String postRegister(@RequestParam String username,@RequestParam String password) {
+
+		
+		//Insert to the user to the DB
+		da.addUser(username, password);
+		
+		// Find the userid for the new user
+		Long userId = da.findUserAccount(username).getUserID(); 
+		
+		//Assign the new user to roles Admin and User
+		da.addRole(userId, Long.valueOf(1));//Admin 
+		da.addRole(userId, Long.valueOf(2));//user
+		
+		
+		//Redirect to index page
+		
+		return"redirect:/";
+		
+		
+	}
+	
 	
 	
 }
--- src/main/java/MidtermAMohamed/security/SecurityConfig.java
+++ src/main/java/MidtermAMohamed/security/SecurityConfig.java
@@ -1,91 +1,92 @@
 package MidtermAMohamed.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import MidtermAMohamed.services.UserDetailsServiceImpl;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private LoginAccessDeniedHandler accessDeniedHandler;
-	
-	
-	
-//Users with role (User) get access to all the resources and everything under secure as well.
-protected void configure(HttpSecurity http) throws Exception {
-http.authorizeRequests()
-.antMatchers("/secure/**").hasRole("USER")
-
-//everything under resources including the folders: js,cs,images...
-.antMatchers("/", "/js/**", "/css/**", "/images/**", "/**").permitAll()
-.anyRequest().authenticated()
-.and().formLogin()
-
-//Custom login form (/login)
-.loginPage("/login").permitAll()
-
-// When the user logs out
-.and().logout()
-
-//invalidate the session
-.invalidateHttpSession(true)
-
-//Clear credentials/Authentication info
-.clearAuthentication(true)
-
-.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-.logoutSuccessUrl("/login?logout").permitAll()
-
-
-.and().exceptionHandling()
-.accessDeniedHandler(accessDeniedHandler);
-}
-	
-
-protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-auth.inMemoryAuthentication()
-.passwordEncoder(NoOpPasswordEncoder.getInstance())
-.withUser("frank@frank.com").password("1234").roles("USER")
-.and()
-.withUser("guest@guest.com").password("password").roles("GUEST");
-}
 
 
 
+	@Autowired
+	private UserDetailsServiceImpl userDetailsService;
 
 
 
+	//Users with role (User) get access to all the resources and everything under secure as well.
+	protected void configure(HttpSecurity http) throws Exception {
+
+
+		http.csrf().disable();
+		http.headers().frameOptions().disable();
+
+		http.authorizeRequests()
+
+
+		.antMatchers(HttpMethod.POST, "/register").permitAll()
+
+
+		.antMatchers("/secure/**").hasRole("USER")
+
+
+		//everything under resources including the folders: js,cs,images...
+		.antMatchers("/", "/js/**", "/css/**", "/images/**", "/**").permitAll()
+
+		//
+
+
+		//Specify db to use
+		.antMatchers("/h2-console/**").permitAll()
+		.anyRequest().authenticated()
+		.and().formLogin()
+
+		//Custom login form (/login)
+		.loginPage("/login").permitAll()
+
+		// When the user logs out
+		.and().logout()
+
+		//invalidate the session
+		.invalidateHttpSession(true)
+
+		//Clear credentials/Authentication info
+		.clearAuthentication(true)
+
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+		.logoutSuccessUrl("/login?logout").permitAll()
+
+		.and().exceptionHandling()
+		.accessDeniedHandler(accessDeniedHandler);
+	}
+	//End of sec method	
 
 
 
+	//To handle encoded user name and password
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}//End of crypt method
 
 
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth)
+			throws Exception{
+		//////
+		auth.userDetailsService(userDetailsService)
+		.passwordEncoder(passwordEncoder());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-	
-	
+	}
 }
